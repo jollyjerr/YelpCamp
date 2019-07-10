ade7bf091eacb3750ce5ef057ef0e5380c8ff119
jollyjerr <jollyjerr@gmail.com>
2019-07-10 10:28:43 -0600

    set up show route

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -18,7 +19,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 //TESTS
 // Campground.create({
 //     name: "Circle Park Campground",
-//     image: "https://static1.squarespace.com/static/570cea31859fd01b8faced0c/t/5775c8452e69cff5b8fe568c/1467336807087/http%3A%2F%2Fwww.the-openroad.com+Big+Horn+National+Forest"
+//     image: "https://static1.squarespace.com/static/570cea31859fd01b8faced0c/t/5775c8452e69cff5b8fe568c/1467336807087/http%3A%2F%2Fwww.the-openroad.com+Big+Horn+National+Forest",
+//     description: "Circle Park Campground is an expansive hilly landscape with great mountain views. Be sure to drive around and look for the perfect campsite! Bring your own water!"
 // }, function(err, campground) {
 //     if (err) {
 //         console.log(err);
@@ -34,20 +36,23 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+//index - display
 app.get("/campgrounds", function(req, res) {
     Campground.find({}, function(err, allCampgrounds) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds", { campgrounds: allCampgrounds });
+            res.render("index", { campgrounds: allCampgrounds });
         }
     });
 });
 
+//create - send form
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = { name: name, image: image };
+    var description = req.body.description;
+    var newCampground = { name: name, image: image, description: description };
     //push to database
     Campground.create(newCampground, function(err, newlyCamp) {
         if (err) {
@@ -59,9 +64,25 @@ app.post("/campgrounds", function(req, res) {
     });
 });
 
+//new - display form
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
 });
+
+//show - info for one campground
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            //render show template
+            res.render("show", { campground: foundCampground });
+        }
+    });
+});
+
+
+
 
 app.listen(3000, function() {
     console.log("YelpCamp is running on port 3000");
