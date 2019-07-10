9615876b04c59b50e1446a815e7791c2d20ac2d5
jollyjerr <jollyjerr@gmail.com>
2019-07-09 22:37:23 -0600

    link submit form to database

--- app.js
+++ app.js
@@ -16,40 +16,47 @@ var campgroundSchema = new mongoose.Schema({
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 //TESTS
-Campground.create({
-    name: "Circle Park Campground",
-    image: "https://static1.squarespace.com/static/570cea31859fd01b8faced0c/t/5775c8452e69cff5b8fe568c/1467336807087/http%3A%2F%2Fwww.the-openroad.com+Big+Horn+National+Forest"
-}, function(err, campground) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Newly created campground: ");
-        console.log(campground);
-    }
-});
+// Campground.create({
+//     name: "Circle Park Campground",
+//     image: "https://static1.squarespace.com/static/570cea31859fd01b8faced0c/t/5775c8452e69cff5b8fe568c/1467336807087/http%3A%2F%2Fwww.the-openroad.com+Big+Horn+National+Forest"
+// }, function(err, campground) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Newly created campground: ");
+//         console.log(campground);
+//     }
+// });
 
 //ROUTES
-var campgrounds = [
-    { name: "South Fork Campground", image: "http://4.bp.blogspot.com/-wCDfRat_Nlo/U-UGlxdwi9I/AAAAAAAABSE/qqFJIWleunQ/s1600/01.JPG" },
-    { name: "Circle Park Campground", image: "https://static1.squarespace.com/static/570cea31859fd01b8faced0c/t/5775c8452e69cff5b8fe568c/1467336807087/http%3A%2F%2Fwww.the-openroad.com+Big+Horn+National+Forest" },
-    { name: "Sitting Bull Campground", image: "https://www.fs.usda.gov/Internet/FSE_MEDIA/stelprd3844268.jpg" }
-];
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", { campgrounds: campgrounds });
+    Campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", { campgrounds: allCampgrounds });
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = { name: name, image: image };
-    campgrounds.push(newCampground);
-    //redirect to the campgrounds page
-    res.redirect("/campgrounds");
+    //push to database
+    Campground.create(newCampground, function(err, newlyCamp) {
+        if (err) {
+            console.log(err);
+        } else {
+            //redirect to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res) {
